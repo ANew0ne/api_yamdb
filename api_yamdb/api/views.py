169a427c6a1bb3d5b3239baf19c7dac2e8e64a9a
aleksei_astapov <Alexey.Astapoff@yandex.ru>
2023-12-11 18:57:02 +0300

    Model, views, siriallizers, url review-comment-rating

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from .serializers import CommentSerializer, ReviewSerializer
+from reviews.models import Review, Title
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    '''Вьюсет для Комментариев'''
+
+    serializer_class = CommentSerializer
+    # permission_classes =
+
+    def get_queryset(self):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    '''Вьюсет для Отзывов'''
+
+    serializer_class = ReviewSerializer
+    # permission_classes =
+
+    def get_title(self):
+        return get_object_or_404(
+            Title, id=self.kwargs.get('title_id')
+        )
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, title=self.get_title())
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,95 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from users.model import User
+
+MAX_LENGTH = 200
+TEXT_LIMIT_SHOW = 20
+
+
+class Review(models.Model):
+    '''Модель Отзывов'''
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение'
+    )
+    text = models.CharField(
+        max_length=MAX_LENGTH
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    score = models.IntegerField(
+        'Оценка',
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ),
+        error_messages={'validators': 'Диапазон от 1 до 10!'}
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        default_related_name = 'reviews'
+        ordering = ('author', '-pub_date')
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author', ),
+                name='unique review'
+            )]
+
+    def __str__(self):
+        return (
+            f'Title: {self.title[:TEXT_LIMIT_SHOW]}, '
+            f'Text: {self.text[:TEXT_LIMIT_SHOW]}, '
+            f'Author: {self.author}, '
+            f'Date: {self.pub_date}, '
+            f'Score: {self.score}'
+        )
+
+
+class Comment(models.Model):
+    '''Модель Комментариев'''
+
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        verbose_name='Отзыв'
+    )
+    text = models.CharField(
+        'Текст комментария',
+        max_length=MAX_LENGTH
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        default_related_name = 'comments'
+        ordering = ('author', '-pub_date')
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return (
+            f'Review: {self.review[:TEXT_LIMIT_SHOW]}, '
+            f'Text: {self.text[:TEXT_LIMIT_SHOW]}, '
+            f'Author: {self.author}, '
+            f'Date: {self.pub_date}, '
+        )
