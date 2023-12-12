61b644b4dd6a7fe776f2c79fc1d57f3b9b73bbe2
Denis Poddubny <dgreenpie@gmail.com>
2023-12-12 19:30:16 +0300

    changed User model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,21 +2,31 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from .enums import UserRole
-MAX_LENGTH = 100
+MAX_FIELD_LENGTH = 150
+MAX_EMAIL_LENGTH = 254
 
 
 class User(AbstractUser):
     email = models.EmailField(verbose_name='Электронная почта',
                               unique=True,
-                              max_length=MAX_LENGTH)
+                              max_length=MAX_EMAIL_LENGTH)
     username = models.CharField(verbose_name='Имя пользователя',
                                 unique=True,
-                                max_length=MAX_LENGTH)
-    confirmation_code = models.CharField(max_length=MAX_LENGTH)
+                                max_length=MAX_FIELD_LENGTH)
+    first_name = models.CharField(verbose_name='Имя',
+                                  max_length=MAX_FIELD_LENGTH,
+                                  null=True, blank=True)
+    last_name = models.CharField(verbose_name='Фамилия',
+                                 max_length=MAX_FIELD_LENGTH,
+                                 null=True, blank=True)
+    bio = models.TextField(verbose_name='О себе',
+                           null=True, blank=True)
     role = models.CharField(verbose_name='Роль',
-                            max_length=MAX_LENGTH,
                             choices=UserRole.choices(),
-                            default=UserRole.USER)
+                            default=UserRole.USER,
+                            max_length=MAX_FIELD_LENGTH)
+
+    REQUIRED_FIELDS = ('email',)
 
     class Meta:
         verbose_name = 'Пользователь'
