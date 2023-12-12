269141e1589dfcdd71323249ca9b85c89833259d
aleksei_astapov <Alexey.Astapoff@yandex.ru>
2023-12-12 10:34:49 +0300

    modernization CommentViewSet Readme

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -11,17 +11,16 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     # permission_classes =
 
+    def get_review(self):
+        return get_object_or_404(
+            Review, id=self.kwargs.get('review_id')
+        )
+
     def get_queryset(self):
-        review = get_object_or_404(
-            Review,
-            id=self.kwargs.get('review_id'))
-        return review.comments.all()
+        return self.get_review().comments.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(
-            Review,
-            id=self.kwargs.get('review_id'))
-        serializer.save(author=self.request.user, review=review)
+        serializer.save(author=self.request.user, review=self.get_review())
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
