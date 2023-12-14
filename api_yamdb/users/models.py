f103a0e7eac9f0c7b0045886151bf57b5a41460d
Denis Poddubny <dgreenpie@gmail.com>
2023-12-14 19:04:09 +0300

    17f-63p

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -33,7 +33,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-        ordering = ('last_name', 'first_name')
+        # ordering = ('last_name', 'first_name')
 
     @property
     def is_admin(self):
